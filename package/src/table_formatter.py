b8cb9107b2f8607fec799cf4d4c8b838ea9c7a36
carolus44 <152527316+carolus44@users.noreply.github.com>
2023-12-01 11:15:27 +0100

    Update table_formatter.py
    simplified stuff

--- package/src/table_formatter.py
+++ package/src/table_formatter.py
@@ -136,30 +136,16 @@ def natural_sort(l):
     alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
     return sorted(l, key=alphanum_key)
 
-
-# Return a tuple (r, g, b) from an integer color
 def get_rgb_from_color(color):
-    red = color >> 16
-    color_updated = color - red * 65536
-    green = color_updated >> 8
-    blue = color_updated - green * 256
-    return (red, green, blue)
+    red_green, blue = divmod(color, 256)
+    return *divmod(red_green, 256), blue
 
-
-# Convert a integer color to a dictionary
 def color_to_dict(color):
-    components = get_rgb_from_color(color)
-    rgb_dict = dict()
-    rgb_dict["red"] = components[0]
-    rgb_dict["green"] = components[1]
-    rgb_dict["blue"] = components[2]
-    return rgb_dict
+    return dict(zip(("red","green","blue"),get_rgb_from_color(color)))
 
-
-# Return (r, g, b) values from RGB dict
 def rgb_from_dict(color_dict):
-    return color_dict["red"], color_dict["green"], color_dict["blue"]
-
+    r, g, b = colordict.values()
+    return r, g, b
 
 # Get the format of a single border
 def get_border_format(border):
